080acecbdc47329498b73edcfb897b2d07d0640c
shangzonghai <shangzonghai@youzan.com>
2018-07-23 13:08:45 +0800

    dump1

---
+++ chap1/dump1/Dump1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"fmt"
+)
+
+func main()  {
+	input := bufio.NewScanner(os.Stdin)
+	words := make(map[string]int)
+	for input.Scan() {
+		fmt.Println("Received:" + input.Text())
+		words[input.Text()]++
+	}
+
+	for word, n := range words {
+		if n > 1 {
+			fmt.Printf("%d %s\n", n, word)
+		}
+	}
+}
